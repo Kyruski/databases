ca0283ae41cf1f2ac7bfd2449550452b63c0d492
Kyruski <jamescboyett@gmail.com>
2019-01-23 16:21:53 -0600

    Implements server/schema.sql with created tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE usernames (
+  id integer primary key,
+  username text not null
+);
+
+CREATE TABLE rooms (
+  id integer primary key,
+  roomname text not null
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer primary key,
+  messagetext text,
+  username integer,
+  room integer,
+  -- created_At timestamp,
+  -- updated_At timestamp,
+  foreign key (username) REFERENCES usernames(id),
+  foreign key (room) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
